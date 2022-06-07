c48012e09b39c431c26bce30b68a4033d0d867c1
Michal Bojanowski <michal2992@gmail.com>
2022-06-07 08:46:29 +0200

    Remove gdrive aliases
    I don't want to use it. Use 'rclone' instead.

--- aliases/gdrive.bash
+++
@@ -1,2 +0,0 @@
-alias gdrive-upload="gdrive sync upload ~/Drive/Szyfry 1dMfJ7wgEUJr9wwgpoU0eryrw_SfpRpHj"
-alias gdrive-download="gdrive sync download 1dMfJ7wgEUJr9wwgpoU0eryrw_SfpRpHj ~/Drive/Szyfry"
