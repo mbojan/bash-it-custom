552e8eeff97aa592649153bc1874c5b7e66b0d4b
Michal Bojanowski <michal2992@gmail.com>
2023-01-27 12:29:39 +0100

    Add 'sqlen' alias

--- aliases/slurm.bash
+++ aliases/slurm.bash
@@ -4,3 +4,5 @@ about-alias 'Aliases for SLURM workload manager'
 # Relevant queue
 alias myq='squeue -u $USER --format="%.10i %.9P %.16E %.30j %.8u %.10M %.6D %.3t %R"'
 
+# How many jobs wait for resources
+alias sqlen='squeue | grep " PD " | wc -l'
