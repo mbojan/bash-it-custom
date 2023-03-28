69a55d0ef3cfd4af9c3471105990914e252ca862
Michal Bojanowski <michal2992@gmail.com>
2023-03-28 10:58:52 +0200

    Add aliases for push/pull keepassxc stuff

---
+++ aliases/keepassxc.bash
@@ -0,0 +1,5 @@
+cite 'about-alias'
+about-alias 'Michals KeepassXC aliases'
+
+alias keepassxc-push='rclone sync -v ~/Drive/Szyfry gd:Szyfry'
+alias keepassxc-pull='rclone sync -v gd:Szyfry ~/Drive/Szyfry'
