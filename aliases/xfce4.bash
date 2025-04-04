246e2b8d7c277b94117527e175b51e41c0ce8695
Michał Bojanowski <michal2992@gmail.com>
2025-04-04 13:34:12 +0200

    Add aliases for managing xfce4 panel

---
+++ aliases/xfce4.bash
@@ -0,0 +1,2 @@
+alias monitors-a11="xfce4-panel-profiles load /home/mbojan/.local/share/xfce4-panel-profiles/A11.tar.bz2"
+alias monitors-laptop="xfce4-panel-profiles load /home/mbojan/.local/share/xfce4-panel-profiles/Laptop.tar.bz2"
