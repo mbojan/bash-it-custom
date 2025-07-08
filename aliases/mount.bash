eff7fe4694844d1b0ba04fc8c52d87517150dfa2
Michał Bojanowski <michal2992@gmail.com>
2025-07-08 13:55:30 +0200

    Add mounting UAB PATCHWORK folder

--- aliases/mount.bash
+++ aliases/mount.bash
@@ -1,4 +1,7 @@
 # Commands mounting some remote drives
 
 # Mount DVC remote at UAB's OneDrive share
-alias mount-dvc-uab="rclone mount uab:DVC ~/mnt/DVC --vfs-cache-mode full --daemon"
+alias mount-uab-dvc="rclone mount uab:DVC ~/mnt/DVC --vfs-cache-mode full --daemon"
+
+# Mount UAB PATCHWORK folder
+alias mount-uab-patchwork="rclone mount uab:PATCHWORK ~/mnt/PATCHWORK --vfs-cache-mode full --daemon"
