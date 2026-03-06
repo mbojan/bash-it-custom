07d1b3a086bf2ff2a7d26f676b4d392236ed189e
Michał Bojanowski <michal2992@gmail.com>
2026-03-06 10:40:23 +0100

    Add checking timestamps

--- aliases/keepassxc.bash
+++ aliases/keepassxc.bash
@@ -3,3 +3,4 @@ about-alias 'Michals KeepassXC aliases'
 
 alias keepassxc-push='rclone sync -v ~/Drive/Szyfry gd:Szyfry'
 alias keepassxc-pull='rclone sync -v gd:Szyfry ~/Drive/Szyfry'
+alias keepassxc-check='echo Local; rclone lsl ~/Drive/Szyfry; echo Remote; rclone lsl gd:Szyfry'
