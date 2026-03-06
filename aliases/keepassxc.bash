bdc5f3d31e21e23887ceddcc8c1a2320d6d7291d
Michał Bojanowski <michal2992@gmail.com>
2026-03-06 10:42:10 +0100

    Add rclone check too

--- aliases/keepassxc.bash
+++ aliases/keepassxc.bash
@@ -3,4 +3,4 @@ about-alias 'Michals KeepassXC aliases'
 
 alias keepassxc-push='rclone sync -v ~/Drive/Szyfry gd:Szyfry'
 alias keepassxc-pull='rclone sync -v gd:Szyfry ~/Drive/Szyfry'
-alias keepassxc-check='echo Local; rclone lsl ~/Drive/Szyfry; echo Remote; rclone lsl gd:Szyfry'
+alias keepassxc-check='rclone check ~/Drive/Szyfry gd:Szyfry --combined -; echo Local; rclone lsl ~/Drive/Szyfry; echo Remote; rclone lsl gd:Szyfry'
