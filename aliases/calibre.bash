8a2c4703b932ad8274cd11b7537644ba0ca1754a
Michał Bojanowski <michal2992@gmail.com>
2026-09-01 12:36:28 +0200

    Rename aliases installing calibre and rclone

--- aliases/calibre.bash
+++ aliases/calibre.bash
@@ -1,1 +1,1 @@
-alias install_calibre="sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin"
+alias install-calibre="sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin"
